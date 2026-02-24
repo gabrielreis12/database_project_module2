60432cf5a7ff79d9ded6ff3cc38d6ba5fd29b7df
Gabriel Reis <gabrieladriano.jr@gmail.com>
2026-02-24 01:15:55 -0300

    created and tested a generic function (that can be used for other entities) for SQL UPDATE commands

--- comandos_tabela_aluno.py
+++ comandos_tabela_aluno.py
@@ -1,89 +1,45 @@
 import sqlite3
 import criacao_do_banco
 
+# d
+
 # pensei em utilizar o kwargs** para deixar o código mais flexível na mudança de informações do banco
 
-def atualizar_curso_aluno(matricula_id, novo_id_curso):
-    con = sqlite3.connect("escola.db")
+def atualizar_atributo(TableName, AttributeType, NewAttributeValue, EntityID):
+
+    con = sqlite3.connect("escola.db",timeout=10)
     cur = con.cursor()
     
     # Ativamos a checagem de Chave Estrangeira (obrigatório no SQLite)
     cur.execute("PRAGMA foreign_keys = ON;")
+
+    # utilizando duas variaveis como condicional para que os valores dos parâmetros da funcao atualizar_atributo sejam apenas os reais nomes dos atributos e das tabelas e não um comando em SQL que consulte dados, ou exclua os dados do BD
+    VariableForCondition1 = AttributeType in criacao_do_banco.possiveis_valores_atributos
+    VariableForCondition2 = TableName in criacao_do_banco.possiveis_valores_tabelas
     
-    # o bloco try/except existe para previnir a procura de um ID inexistente nesse caso
-    try:
-        cur.execute("""
-            UPDATE Aluno
-            SET ID_curso = ?
-            WHERE matricula_ID = ?
-        """, (novo_id_curso, matricula_id))
-        
-        # O rowcount nos diz quantas linhas o UPDATE afetou
-        if cur.rowcount == 0:
-            print(f"Erro: Nenhum aluno encontrado com a matrícula {matricula_id}.")
-        else:
-            con.commit()
-            print("Curso do aluno atualizado com sucesso!")
+    if VariableForCondition1 and VariableForCondition2:
+
+        # o bloco try/except existe para previnir a procura de um ID inexistente nesse caso
+        try:
+            cur.execute(f"UPDATE {TableName} SET {AttributeType} = ? WHERE matricula_ID = ?", (NewAttributeValue, EntityID))
             
-    except sqlite3.IntegrityError:
-        # Se tentar colocar um ID_curso que não existe na tabela Curso, cai aqui!
-        print(f"Erro: O curso de ID {novo_id_curso} não existe no sistema.")
-        
-    finally:
-        con.close()
-
-def atualizar_nome_aluno(matricula_id, novo_nome_aluno):
-    con = sqlite3.connect("escola.db")
-    cur = con.cursor()
-    
-    # Ativamos a checagem de Chave Estrangeira (obrigatório no SQLite)
-    cur.execute("PRAGMA foreign_keys = ON;")
-    
-    cur.execute("""
-    UPDATE Aluno
-    SET Nome = ? 
-    WHERE matricula_ID = ?
-    """, (novo_nome_aluno, matricula_id))  
-    # # notar ordem dos parâmetros nome_nome_aluno vem antes de matricula. a ordem importa já que a primeira interrogação se refere ao atributo Nome da tabela Aluno, enquanto a segunda interrogação é acerca do matricula_ID e é PK    
-        # O rowcount nos diz quantas linhas o UPDATE afetou
-    if cur.rowcount == 0:
-        print(f"Erro: Nenhum aluno encontrado com a matrícula {matricula_id}.")
-    else:
-        con.commit()
-        print("Nome do aluno atualizado com sucesso!")
-            
-    con.close()
-
-def atualizar_CPF_aluno(matricula_id, novo_CPF_aluno):
-
-    confirmando_se_quer_mudar_o_CPF = input('Você tem certeza que o atributo que deseja mudar é o CPF?').capitalize()
-    print('PRESSIONE a tecla A) para SIM, PRESSIONE a tecla B) para NÃO')
-    print('A) Sim        B) Não')
-    # aqui, quis adicionar uma camada de segurança para o BD, no sentido de que normalmente não se muda o dado CPF, mas, definitivamente é uma função que precisa existir
-    if confirmando_se_quer_mudar_o_CPF == 'A':
-
-        con = sqlite3.connect("escola.db",timeout=10)
-        cur = con.cursor()
-        
-        # Ativamos a checagem de Chave Estrangeira (obrigatório no SQLite)
-        cur.execute("PRAGMA foreign_keys = ON;")
-
-        cur.execute("""
-        UPDATE Aluno
-        SET CPF = ? 
-        WHERE matricula_ID = ?
-        """, (novo_CPF_aluno, matricula_id))  
-        # # notar ordem dos parâmetros nome_nome_aluno vem antes de matricula. a ordem importa já que a primeira interrogação se refere ao atributo Nome da tabela Aluno, enquanto a segunda interrogação é acerca do matricula_ID e é PK    
             # O rowcount nos diz quantas linhas o UPDATE afetou
-        if cur.rowcount == 0:
-            print(f"Erro: Nenhum aluno encontrado com a matrícula {matricula_id}.")
-        else:
-            con.commit()
-            print("CPF do aluno atualizado com sucesso!")
-        cur.close()       
-        con.close()
+            if cur.rowcount == 0:
+                print(f"Erro: Nenhum/Nenhuma {TableName} encontrado com o ID num.: {EntityID} informado.")
+            else:
+                con.commit()
+                print("Dado atualizado com sucesso!")
+                
+        except sqlite3.IntegrityError:
+            # Se tentar colocar um ID_curso que não existe na tabela Curso, cai aqui!
+            print(f"Erro: A entidade {TableName} com {EntityID} não existe no sistema.")
+            
+        finally:
+            con.close()
+    
     else:
-        print('Obrigado por confirmar!')
+        print('Perigo! Risco de vulnerabilidade!')
+    
 
 def inserir_aluno(matricula_id, nome_curso, cpf_curso, dob_curso, nome_pai_curso, nome_mae_curso, id_curso):
     con = sqlite3.connect("escola.db",timeout=10)
@@ -97,6 +53,31 @@ def inserir_aluno(matricula_id, nome_curso, cpf_curso, dob_curso, nome_pai_curso
             INSERT INTO Aluno 
             VALUES (?,?,?,?,?,?,?) 
             """, (matricula_id, nome_curso, cpf_curso, dob_curso, nome_pai_curso, nome_mae_curso, id_curso))
+        con.commit()
+        print('Salvo com sucesso!')
+        
+    except sqlite3.IntegrityError as erro:
+    # Se cair aqui, ou a matrícula já existe, ou o ID do curso é inválido!
+        print("Erro ao inserir aluno! (funcao inserir aluno)")
+        print(f"Detalhe: Verifique se a matrícula {matricula_id} já existe ou se o curso {id_curso} é válido.")
+        print(f"Aviso do banco de dados: {erro}")
+    finally:
+        cur.close()
+        con.close()
+
+def delete_aluno(matricula_id):
+
+    con = sqlite3.connect("escola.db",timeout=10)
+    cur = con.cursor()
+        
+    # Ativamos a checagem de Chave Estrangeira (obrigatório no SQLite)
+    cur.execute("PRAGMA foreign_keys = ON;")
+    try:
+
+        cur.execute("""
+            DELETE FROM Aluno 
+            WHERE matricula_ID = ?
+            """, (matricula_id,))
         con.commit()
         print('Salvo com sucesso!')
         
